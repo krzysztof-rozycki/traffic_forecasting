0767fc32731986434cb1d3ff451bf7f322ca3da7
Krzysztof Rozycki <krzysztof_rozycki@epam.com>
2025-01-10 16:52:29 +0100

    adding preprocessing function that deals with some duplications of rows and multiple values of weather input in multiple rows for a given date_time record

--- forecasting/dataset.py
+++ forecasting/dataset.py
@@ -7,6 +7,18 @@ def import_data(file_path: str):
     return pd.read_csv(file_path)
 
 
+def preprocess_data(df: pd.DataFrame):
+    df_modified = pd.get_dummies(df, columns=['weather_main', 'weather_description'])
+
+    # change format of dummy variables from bool to numeric and combine into one data frame
+    weather_dummies_columns = df_modified.filter(regex='^weather').columns
+    df_dummies = df_modified[weather_dummies_columns].astype(int)
+    df_no_dummies = df_modified.drop(columns=weather_dummies_columns)
+    df_full = pd.concat([df_no_dummies, df_dummies], axis=1)
+
+    return df_full
+
+
 def make_train_test_split(X: pd.DataFrame, y: pd.Series):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y,
