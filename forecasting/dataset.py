b96b0643a8d2ed6374b5449e58981f30530e657f
Krzysztof Rozycki <krzysztof_rozycki@epam.com>
2025-01-10 14:06:23 +0100

    Added train test split to dataset module

--- forecasting/dataset.py
+++ forecasting/dataset.py
@@ -1,5 +1,17 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from forecasting.utils import train_config
 
 
 def import_data(file_path: str):
     return pd.read_csv(file_path)
+
+
+def make_train_test_split(X: pd.DataFrame, y: pd.Series):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        train_size=train_config['train_size'],
+        shuffle=False,
+        # random_state=train_config['random_state']
+    )
+    return X_train, X_test, y_train, y_test
