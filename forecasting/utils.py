61ebd44416d0641067529370dd9a495ebf0a1883
Krzysztof Rozycki <krzysztof_rozycki@epam.com>
2025-01-12 20:58:18 +0100

    Modified config to automatically add PROJECT_ROOT to the paths

--- forecasting/utils.py
+++ forecasting/utils.py
@@ -3,6 +3,8 @@ from numpy import sin, cos, pi
 import pandas as pd
 import yaml
 import pickle
+from pathlib import Path
+import os
 
 
 def load_yaml(file_path: str):
@@ -10,10 +12,19 @@ def load_yaml(file_path: str):
         config = yaml.safe_load(f)
     return config
 
+
+def adjust_path_to_project_root(config, project_root):
+    return {key: (os.path.join(project_root, value) if key.endswith('path') else value)
+            for key, value in config.items()}
+
+
 features_config = load_yaml('../configs/features.yaml')
 models_config = load_yaml('../configs/models.yaml')
 random_search_config = load_yaml('../configs/random_search.yaml')
+
 train_config = load_yaml('../configs/train.yaml')
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+train_config = adjust_path_to_project_root(train_config, PROJECT_ROOT)
 
 
 def sin_cycle(x: np.ndarray, period: int):
