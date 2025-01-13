0994e0b4f050eb6a6e76b3ee9d0655031a6dd2a5
Krzysztof Rozycki <krzysztof_rozycki@epam.com>
2025-01-13 18:26:47 +0100

    adding docstrings to the functions

--- forecasting/modelling/model_selection.py
+++ forecasting/modelling/model_selection.py
@@ -5,6 +5,20 @@ from forecasting.utils import models_config, random_search_config
 
 
 def baseline_models_estimation(X: pd.DataFrame, y: pd.Series, cv_splits=3, scoring='neg_mean_squared_error'):
+    """
+    Estimates baseline models (based on models.yaml config) with default parameters. This is done to get a rough idea
+    of which algorithm is more promising in estimating traffic volume problem. Later, top n performing models
+    will be used in hyperparameters optimization to get the final model.
+
+    Parameters:
+        X (pd.DataFrame): A Pandas DataFrame containing (raw) features data.
+        y (pd.Series): A Pandas Series containing target variable.
+        cv_splits (int): An integer indication number of folds used in cross validation
+        scoring (string): scoring function used to rank models, defaults to "neg_mean_squared_error"
+
+    Returns:
+        Dict: model_name: avg cv score, sorted by descending order.
+    """
     cv_strategy = KFold(n_splits=cv_splits, shuffle=True, random_state=42)
 
     results = {}
@@ -19,13 +33,25 @@ def baseline_models_estimation(X: pd.DataFrame, y: pd.Series, cv_splits=3, scori
 
 
 def make_random_search(models: list, X: pd.DataFrame, y: pd.Series, scoring='neg_mean_squared_error'):
+    """
+    Runs random search for provided models return best value of scoring function for each model.
+
+    Parameters:
+        models (list): A list of models names. Must be one of models.yaml, otherwise it is not implemented.
+        X (pd.DataFrame): A Pandas DataFrame containing (raw) features data.
+        y (pd.Series): A Pandas Series containing target variable.
+        scoring (string): scoring function used to rank models, defaults to "neg_mean_squared_error"
+
+    Returns:
+        Dict: model_name: RandomizedSearchCV (fitted).
+    """
     results = {}
     for model_name in models:
         print(f"Running random search for {model_name}")
         training_pipeline = make_training_pipeline(model_name)
         search_space = models_config.get(model_name)
-
-        cv_strategy = TimeSeriesSplit(n_splits=random_search_config['cv'], test_size=365*24)
+        test_size = random_search_config['cv_test_size_days'] * 24  # hourly data require multiplication by 24 to get number of records
+        cv_strategy = TimeSeriesSplit(n_splits=random_search_config['cv'], test_size=test_size)
 
         # adjusting search space keys to reflect the structure of the training pipeline
         name_of_model_step = training_pipeline.steps[-1][0]
