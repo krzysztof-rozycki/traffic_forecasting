222333b99b934055d6bc324ceafc82c36bd7cb07
Krzysztof Rozycki <krzysztof_rozycki@epam.com>
2025-01-15 10:33:31 +0100

    Removed redundant lines from utils.py

--- forecasting/utils.py
+++ forecasting/utils.py
@@ -3,7 +3,6 @@ from numpy import sin, cos, pi
 import pandas as pd
 import yaml
 import pickle
-from pathlib import Path
 import os
 
 
@@ -18,22 +17,14 @@ def adjust_path_to_project_root(config, project_root):
             for key, value in config.items()}
 
 
-<<<<<<< Updated upstream
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
-features_config = load_yaml(os.path.join(PROJECT_ROOT, 'configs/features.yaml'))
-models_config = load_yaml(os.path.join(PROJECT_ROOT, 'configs/models.yaml'))
-random_search_config = load_yaml(os.path.join(PROJECT_ROOT, 'configs/random_search.yaml'))
-=======
 def get_current_file_directory():
     config_path = os.getenv('FORECASTING_CONFIG_PATH')
     return config_path
 
 config_path = get_current_file_directory()
-print(f"PROJECT_ROOT: {config_path}")
 features_config = load_yaml(os.path.join(config_path, 'configs/features.yaml'))
 models_config = load_yaml(os.path.join(config_path, 'configs/models.yaml'))
 random_search_config = load_yaml(os.path.join(config_path, 'configs/random_search.yaml'))
->>>>>>> Stashed changes
 
 train_config = load_yaml(os.path.join(config_path, 'configs/train.yaml'))
 train_config = adjust_path_to_project_root(train_config, config_path)
